fef12a9525a419addbb7f50b7d277ea776208965
xblspain-glitch <xblspain@gmail.com>
2026-07-24 09:16:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4093,7 +4093,7 @@ async function shareVerseCard(cardStyle="classic"){
         ctx.restore();
       }
     }
-    await drawExactLogoWatermark(ctx,540,isThematicV2220?1240:1133,isThematicV2220?650:780);
+    await drawExactLogoWatermark(ctx,540,isThematicV2220?1230:1133,isThematicV2220?650:780);
 
     ctx.textAlign="center";
     ctx.fillStyle="#ffffff";
